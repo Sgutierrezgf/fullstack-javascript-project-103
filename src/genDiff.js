31826894131b3a6492753515f145d94bafdc89b9
sebastian <segut10@hotmail.com>
2025-09-04 13:18:54 -0500

    arraglando extension de archivos

--- src/genDiff.js
+++ src/genDiff.js
@@ -11,8 +11,11 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
     const file1 = getFileData(filepath1);
     const file2 = getFileData(filepath2);
 
-    const data1 = parse(file1, filepath1);
-    const data2 = parse(file2, filepath2);
+    const ext1 = path.extname(filepath1).slice(1);
+    const ext2 = path.extname(filepath2).slice(1);
+
+    const data1 = parse(file1, ext1);
+    const data2 = parse(file2, ext2);
 
     const diffTree = buildDiffTree(data1, data2);
 
--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,11 @@
-import yaml from "js-yaml";
+import yaml from 'js-yaml';
 
 const parse = (data, ext) => {
     switch (ext) {
-        case "json":
+        case 'json':
             return JSON.parse(data);
-        case "yml":
-        case "yaml":
+        case 'yml':
+        case 'yaml':
             return yaml.load(data);
         default:
             throw new Error(`Formato no soportado: ${ext}`);
